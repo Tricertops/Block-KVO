baacc2535330ab818cf1492c058723e88e97002f
Martin Kiss <martin.kiss@me.com>
2012-07-14 13:18:20 +0200

    Library target

---
+++ BlockObserving/BlockObserving.h
@@ -0,0 +1,13 @@
+//
+//  BlockObserving.h
+//  BlockObserving
+//
+//  Created by Martin Kiss on 14.7.12.
+//  Copyright (c) 2012 iMartin Kiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlockObserving : NSObject
+
+@end
---
+++ BlockObserving/BlockObserving.m
@@ -0,0 +1,13 @@
+//
+//  BlockObserving.m
+//  BlockObserving
+//
+//  Created by Martin Kiss on 14.7.12.
+//  Copyright (c) 2012 iMartin Kiss. All rights reserved.
+//
+
+#import "BlockObserving.h"
+
+@implementation BlockObserving
+
+@end
