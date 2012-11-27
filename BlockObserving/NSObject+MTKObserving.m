55037d1f4b7f092106d40e703e938b95b39327ad
Martin Kiss <martin.kiss@me.com>
2012-11-27 11:00:58 +0100

    Comment methods in implementation

--- BlockObserving/NSObject+MTKObserving.m
+++ BlockObserving/NSObject+MTKObserving.m
@@ -21,7 +21,7 @@
 
 #pragma mark Internal
 
-///
+/// Getter for dictionary containing all registered observers for this object. Keys are observed key-paths.
 - (NSMutableDictionary *)blockObservers {
     // Observer is a shadow object that has target this object (`self`) and specific key path.
     // There should never exist two or more observers with the same target AND key path.
@@ -38,7 +38,7 @@
     return blockObservers;
 }
 
-///
+/// Find existing observer or create new for this key-path. Multiple uses of one key-path return the same observer.
 - (MTKObserver *)observerForKeyPath:(NSString *)keyPath {
     // Key path is used as key to retrieve observer.
     MTKObserver *observer = [[self blockObservers] objectForKey:keyPath];
@@ -54,24 +54,25 @@
 
 #pragma mark Property
 
-///
+/// Add observation block to appropriate observer for setting the value.
 - (void)observeProperty:(NSString *)keyPath withBlock:(MTKObservationChangeBlock)observationBlock {
     MTKObserver *observer = [self observerForKeyPath:keyPath];
     [observer addSettingObservationBlock:observationBlock];
 }
 
-///
+/// Copy the block and register it for all given key-paths.
 - (void)observeProperties:(NSArray *)keyPaths withBlock:(MTKObservationChangeBlockMany)observationBlock {
     MTKObservationChangeBlock singleObservationBlock = ^(__weak id weakSelf, id old , id new){
         observationBlock(weakSelf);
     };
+    // This should reduce memroy footprint, the block should have only one copy (not sure how blocks are stored in memory, so this may be useless)
     MTKObservationChangeBlock singleObservationBlockCopy = [singleObservationBlock copy];
     for (NSString *keyPath in keyPaths) {
         [self observeProperty:keyPath withBlock:singleObservationBlockCopy];
     }
 }
 
-///
+/// Register block invoking given selector. Smart detecting of number of arguments.
 - (void)observeProperty:(NSString *)keyPath withSelector:(SEL)observationSelector {
     NSMethodSignature *signature = [self methodSignatureForSelector:observationSelector];
     NSInteger numberOfArguments = [signature numberOfArguments];
@@ -97,7 +98,7 @@
     }];
 }
 
-///
+/// Register the selector for each key-path.
 - (void)observeProperties:(NSArray *)keyPaths withSelector:(SEL)observationSelector {
     for (NSString *keyPath in keyPaths) {
         [self observeProperty:keyPath withSelector:observationSelector];
@@ -108,7 +109,7 @@
 
 #pragma mark Relationship
 
-///
+/// Add observation blocks to appropriate observer. If some block was not specified, use the `changeBlock`.
 - (void)observeRelationship:(NSString *)keyPath
                 changeBlock:(MTKObservationChangeBlock)changeBlock
              insertionBlock:(MTKObservationInsertionBlock)insertionBlock
@@ -131,7 +132,7 @@
     }];
 }
 
-///
+/// Call main `-observeRelationship:...` method with only first argument.
 - (void)observeRelationship:(NSString *)keyPath changeBlock:(MTKObservationChangeBlock)changeBlock {
     [self observeRelationship:keyPath changeBlock:changeBlock insertionBlock:nil removalBlock:nil replacementBlock:nil];
 }
@@ -140,14 +141,14 @@
 
 #pragma mark Mapping
 
-///
+/// Call `-map:to:transform:` with transform block that uses returns the same value, or null replacement.
 - (void)map:(NSString *)sourceKeyPath to:(NSString *)destinationKeyPath null:(id)nullReplacement {
     [self map:sourceKeyPath to:destinationKeyPath transform:^id(id value) {
         return value ?: nullReplacement;
     }];
 }
 
-///
+/// Observe source key-path and set its new value to destination every time it changes. Use transformation block, if specified.
 - (void)map:(NSString *)sourceKeyPath to:(NSString *)destinationKeyPath transform:(id (^)(id))transformationBlock {
     [self observeProperty:sourceKeyPath withBlock:^(__weak id self, id old, id new) {
         id transformedValue = (transformationBlock? transformationBlock(new) : new);
@@ -159,7 +160,7 @@
 
 #pragma Cleanup
 
-///
+/// Called usually from dealloc (may be called at any time). Detach all observers. The associated dictionary is released once the deallocation process finishes.
 - (void)removeAllObservations {
     [[self.blockObservers allValues] makeObjectsPerformSelector:@selector(detach)];
     [self.blockObservers removeAllObjects];
