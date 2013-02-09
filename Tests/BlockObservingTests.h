8b78549a3e7c2ce6c86bf51c59af38f4249cd662
Martin Kiss <martin.kiss@me.com>
2013-02-09 14:34:12 +0100

    Create first tests

---
+++ Tests/BlockObservingTests.h
@@ -0,0 +1,13 @@
+//
+//  BlockObservingTests.h
+//  Tests
+//
+//  Created by Martin Kiss on 9.2.13.
+//  Copyright (c) 2013 iMartin Kiss. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BlockObservingTests : SenTestCase
+
+@end
