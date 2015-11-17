4edcc753424a9ef5933a3bd866e62e6902706aa3
Martin Kiss <martin.kiss@me.com>
2015-11-17 09:54:24 +0100

    Simplify detaching on dealloc, just call -detach

--- Sources/MTKObserver.m
+++ Sources/MTKObserver.m
@@ -60,11 +60,7 @@
 }
 
 - (void)dealloc {
-    // RemoveObserver if it is still attached
-    if (self.attached) {
-        [self.target removeObserver:self forKeyPath:self.keyPath];
-    }
-	//NSLog(@"Observer dealloc %@ %@", self.target, self.keyPath);
+    [self detach];
 }
 
 
